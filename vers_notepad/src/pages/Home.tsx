fbbad78251d974e3604a8f39f9529985c947c4c6
keiinuki <k.inuki@haguruma.co.jp>
2021-08-29 18:00:41 +0900

    ログイン用フォームの入れ物だけ用意

--- vers_notepad/src/pages/Home.tsx
+++ vers_notepad/src/pages/Home.tsx
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom";
+//import { useState, useEffect } from "react";
+//import axios from "axios";
 
 export const Home = () => {
+  //const [post, setPost] = useState<string>("");
+  //useEffect(() => {
+    //axios.get("https://raisetech-memo-api.herokuapp.com/api/memos")
+    //.then((response) => {      
+      //setPost(response.data)
+    //})
+    //.catch((error) => {
+      //console.log(error)
+    //});       
+  //},[]);  
   return (
     <div>
       <h1>ここをHOMEにします</h1>
+      <form>
+        <input type="email" placeholder="e-mailを入力" />
+        <br/>
+        <input type="password" placeholder="パスワードを入力"/>
+        <button type= "button" >サインインしてメモ帳へ</button>
+      </form>
+      <li>ここにデータを表示する？</li>
       <div>
       <Link to="/Notepad">「メモ帳」はこちら</Link>
       </div>
--- vers_notepad/src/pages/Page404.tsx
+++ vers_notepad/src/pages/Page404.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Page404 = () => {  
   return (
     <div>
-      <h1>ページが見つからへんねん</h1>
+      <h1>ページが見つかりません</h1>
       <br />
       <Link to="/">HOMEはこちら</Link>
     </div>
