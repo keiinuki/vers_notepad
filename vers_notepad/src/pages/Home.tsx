a33323c634f057fa9d040caba2881ab04da2f2ca
keiinuki <k.inuki@haguruma.co.jp>
2021-08-29 23:05:27 +0900

    ログイン入力フォーム作成

--- vers_notepad/src/pages/Home.tsx
+++ vers_notepad/src/pages/Home.tsx
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
-//import { useState, useEffect } from "react";
-//import axios from "axios";
+import { useState, useCallback, useEffect } from "react";
+import axios from 'axios';
+import { Redirect } from 'react-router';
 
 export const Home = () => {
-  //const [post, setPost] = useState<string>("");
-  //useEffect(() => {
-    //axios.get("https://raisetech-memo-api.herokuapp.com/api/memos")
-    //.then((response) => {      
-      //setPost(response.data)
-    //})
-    //.catch((error) => {
-      //console.log(error)
-    //});       
-  //},[]);  
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [redirect, setRedirect] = useState<boolean>(false)  
+  
+  const onClickAdd = useCallback(async(e) => {    
+    await axios.post("https://raisetech-memo-api.herokuapp.com/api/login", {
+			email: email,
+			password: password,
+    })
+    setRedirect(true)
+  },[redirect]);
+  
+  if (redirect) {
+    return <Redirect to={'/notepad'} />    
+  }else { 
+    alert("unauthorized")
+    console.log("失敗しました");
+  };
+
   return (
     <div>
       <h1>ここをHOMEにします</h1>
       <form>
-        <input type="email" placeholder="e-mailを入力" />
+        <input type="email" onChange={e => setEmail(e.target.value)} placeholder="e-mailを入力" />
         <br/>
-        <input type="password" placeholder="パスワードを入力"/>
-        <button type= "button" >サインインしてメモ帳へ</button>
+        <input type="password" onChange={e => setPassword(e.target.value)} placeholder="パスワードを入力"/>
+        <button type= "button" onClick={onClickAdd} >サインインしてメモ帳へ</button>
       </form>
       <li>ここにデータを表示する？</li>
       <div>
